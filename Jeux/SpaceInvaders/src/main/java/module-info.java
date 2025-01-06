b52f803ab7a0794dbb1f004d922caba7aa0f472f
Charles Theetten <choracle@outlook.com>
2025-01-06 12:47:56 +0100

    Create module-info.java

---
+++ Jeux/SpaceInvaders/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2025 Charles Theetten
+ * Tous droits réservés.
+ * Ce programme est distribué sous licence CC BY-NC-ND 4.0.
+ */
+
+module spaceinvaders {
+    requires transitive javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports abitodyssey.spaceinvaders;
+
+    opens abitodyssey.spaceinvaders to javafx.fxml;
+}
