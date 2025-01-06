c6744b115d4e2301336b0929ebaa40a743a2f067
Charles Theetten <choracle@outlook.com>
2025-01-06 12:52:43 +0100

    Create module-info.java

---
+++ Jeux/Tetris/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2025 Charles Theetten
+ * Tous droits réservés.
+ * Ce programme est distribué sous licence CC BY-NC-ND 4.0.
+ */
+
+module tetris {
+    requires transitive javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports abitodyssey.tetris;
+
+    opens abitodyssey.tetris to javafx.fxml;
+}
