99a141350a99a0d8dcf6b682fc2efe7f39e8149c
Charles T. <choracle@outlook.com>
2025-01-20 15:07:47 +0100

    Update module-info.java

--- Jeux/Snake/src/main/java/module-info.java
+++ Jeux/Snake/src/main/java/module-info.java
@@ -5,10 +5,8 @@
  */
 
 module snake {
-    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
 
     exports abitodyssey.snake;
 
